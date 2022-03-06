590bbacd77185583c064345f26b1facd6ae521ea
Aaron Ekal <ekalaaronkalale@gmail.com>
2022-03-06 15:49:17 +0300

    add some css

---
+++ js/index.js
@@ -0,0 +1,4 @@
+//creating array to represent the Akan names
+const akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+const akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
