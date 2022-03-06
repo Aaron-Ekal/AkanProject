f52fff0ae111244f48602c5576ae273cd556a382
Aaron Ekal <ekalaaronkalale@gmail.com>
2022-03-06 15:51:25 +0300

    added function to get dob & radio button

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@
 const akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 const akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
+// Collecting data from input
+
+function getDate() {
+    let dateInput = document.getElementById('dob').value;
+    console.log(dateInput);
+    male = document.getElementById('maleRadio'); // collecting from male radio
+    female = document.getElementById('femaleRadio');
+
+   
+}
